348192567ce21d513d9aec089ef6afb4af41562d
Sahil khandelwal <khandelwalsahil09@gmail.com>
2021-08-24 14:26:47 +0530

    unused code removed

--- app.js
+++ app.js
@@ -12,18 +12,10 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(methodOverride("_method"));
 
+
+
+
 app.get("/", function(req, res){
-	res.redirect("/maps");
-
-});
-
-app.get("/fir", function(req, res){
-	res.redirect(path.join(__dirname + '/fir.html'));
-
-});
-
-
-app.get("/maps", function(req, res){
 	res.sendFile(path.join(__dirname + '/MapAndLoc.html'));
 });
 
